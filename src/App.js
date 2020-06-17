0db3e42f58dbae3bfc5e8cbd7bb70160fe958d11
Moongirl1207 <amber.perard@student.howest.be>
2020-06-17 16:08:44 +0200

    try

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Fullscreen from "react-full-screen";
+// import Fullscreen from "react-full-screen";
 import socketIOClient from "socket.io-client";
 import Example from "./Example";
 import "./reset.css";
@@ -9,9 +9,8 @@ const ENDPOINT = "https://evening-caverns-60077.herokuapp.com/";
 // const ENDPOINT = "http://127.0.0.1:8081";
 
 function App() {
-
   // let fullScreenRef = useRef(null);
-  let [fullScreenMode, setfullScreenMode]= useState(false);
+  // let [fullScreenMode, setfullScreenMode] = useState(false);
 
   const socket = socketIOClient(ENDPOINT);
   // const [response, setResponse] = useState("");
@@ -29,20 +28,19 @@ function App() {
   //   // setfullScreenMode(!fullScreenMode);
   // }, []);
 
-  const fullScreenToggler = () => {
-    setfullScreenMode(!fullScreenMode);
-  }
-
+  // const fullScreenToggler = () => {
+  //   setfullScreenMode(!fullScreenMode);
+  // };
 
   return (
     <>
-    <Fullscreen enabled={fullScreenMode}>
-    {/* <div onLoad={fullScreenToggler}> */}
-      <button className="fullscreen__button" onClick={fullScreenToggler}>
+      {/* <Fullscreen enabled={fullScreenMode}> */}
+      {/* <div onLoad={fullScreenToggler}> */}
+      {/* <button className="fullscreen__button" onClick={fullScreenToggler}>
              FullScreen Mode
-          </button>
+          </button> */}
       <div className="wifi__container">
-        <img alt="wifi icon" className="wifi" src="./assets/wifi.svg" ></img>
+        <img alt="wifi icon" className="wifi" src="./assets/wifi.svg"></img>
       </div>
       {/* <button onClick={(e) => goFullScreen()}> enable full screen </button> */}
       {/* <p> */}
@@ -63,8 +61,8 @@ function App() {
         socket={socket}
         name={"carControls"}
       />
-    {/* </div> */}
-    </Fullscreen>
+      {/* </div> */}
+      {/* </Fullscreen> */}
     </>
     // TODO: input eventlistener 'input' needs to trigger emit to server
   );
